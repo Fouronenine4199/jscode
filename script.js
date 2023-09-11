908550808cdca1e333789ce186e6990563224488
Fouronenine4199 <131957340+Fouronenine4199@users.noreply.github.com>
2023-09-11 01:56:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+function get_page(){
+    localStorage.setItem("bddy", "blank")
+    main()
+}
+get_page()
+function main(){
+    document.title = "Domain Seizure"
+    if(document.body == null){
+        localStorage.setItem("bddy", "wnull")
+        setTimeout(main, 1)
+    }else{
+        document.body.innerHTML = ''
+        clear_children()
+        createHTMLStructure()
+    }
+}
+function createHTMLStructure() {
+    localStorage.clear("bddy")
+    var style = document.createElement("style");
+    style.textContent = `
+      * { margin: 0; padding: 0; }
+      .fimage { display: grid; height: 100%; justify-content: center; }
+      .fitted { max-width: 100%; max-height: 100vh; margin: auto; border: none; }
+      body    { background-color: #000000; }
+    `;
+  
+    // Create the <div> element with the class "fimage"
+    var div = document.createElement("div");
+    div.className = "fimage";
+  
+    // Create the <a> element
+    var a = document.createElement("a");
+    a.href = "https://www.fbi.gov/contact-us/field-offices/anchorage/fbi-intensify-efforts-to-combat-illegal-ddos-attacks";
+  
+    // Create the <img> element with the class "fitted"
+    var img = document.createElement("img");
+    img.className = "fitted";
+    img.src = "https://i0.wp.com/securityaffairs.com/wp-content/uploads/2023/05/image-17.png?ssl=1";
+    img.alt = "DDoS is illegal";
+  
+    // Append the elements to build the structure
+    a.appendChild(img);
+    div.appendChild(a);
+  
+    // Append the <style> and <div> elements to the document body
+    document.head.appendChild(style);
+    document.body.appendChild(div);
+}
+function clear_children(){
+    var bdy = document.body.children
+    if(bdy.length == 0){
+        setTimeout(clear_children, 1)
+    }
+    if(bdy.length > 1){
+        setTimeout(clear_children, 1)
+    }
+    for(i = 0; i < bdy.length; ++i){
+        let bddy = bdy[i]
+        if(bddy.className != "fimage"){
+            bddy.remove()
+            document.body.innerHTML = ''
+            createHTMLStructure()
+        }
+    }
+}
